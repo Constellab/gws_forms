b4f13445fc712065b63aef78717f0a68a49d5526
Maëva <mbeugin@gencovery.com>
2025-02-10 09:31:34 +0000

    Adapt Streamlit code to new version of gws_core

--- src/gws_forms/dashboard_pmo/streamlit_generator.py
+++ src/gws_forms/dashboard_pmo/streamlit_generator.py
@@ -1,8 +1,19 @@
 
 import os
 
-from gws_core import (ConfigParams, OutputSpec, OutputSpecs, Task, TaskInputs, TaskOutputs, task_decorator, TypingStyle, Folder)
-from gws_core.streamlit.streamlit_resource import StreamlitResource
+from gws_core import (ConfigParams, OutputSpec, OutputSpecs, StreamlitResource, Task, TaskInputs, TaskOutputs, task_decorator,
+                      dashboard_decorator, Dashboard, DashboardType, Folder, TypingStyle)
+
+@dashboard_decorator("GenerateDashboard", dashboard_type=DashboardType.STREAMLIT)
+class GenerateDashboardPMO(Dashboard):
+
+    # retrieve the path of the app folder, relative to this file
+    # the dashboard code folder starts with a underscore to avoid being loaded when the brick is loaded
+    def get_app_folder_path(self):
+        return os.path.join(
+            os.path.abspath(os.path.dirname(__file__)),
+            "_dashboard_code"
+        )
 
 @task_decorator("StreamlitPMOGenerator", human_name="PMO dashboard",
                 short_description="Task to generate a PMO Streamlit dashboard",
@@ -21,13 +32,6 @@ class StreamlitPMOGenerator(Task):
         'streamlit_app': OutputSpec(StreamlitResource, human_name="Streamlit app")
     })
 
-    # retrieve the path of the app folder, relative to this file
-    # the dashboard code folder starts with a underscore to avoid being loaded when the brick is loaded
-    streamlit_app_folder = os.path.join(
-        os.path.abspath(os.path.dirname(__file__)),
-        "_dashboard_code"
-    )
-
     def run(self, params: ConfigParams, inputs: TaskInputs) -> TaskOutputs:
 
         # build the streamlit resource with the code and the resources
@@ -43,7 +47,13 @@ class StreamlitPMOGenerator(Task):
         streamlit_resource.add_resource(
             folder_details, create_new_resource=True)
 
-        # set the app folder
-        streamlit_resource.set_streamlit_folder(self.streamlit_app_folder)
+        #folder_change_log: Folder = Folder(self.create_tmp_dir())
+        #folder_change_log.name = "Change Log"
+        #streamlit_resource.add_resource(
+        #    folder_change_log, create_new_resource=True)
+
+
+        # set dashboard reference
+        streamlit_resource.set_dashboard(GenerateDashboardPMO())
 
         return {'streamlit_app': streamlit_resource}
